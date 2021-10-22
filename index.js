5a2493ebabc5e123640fffab4187f342a3ad4253
zachary <zacharyberdell@gmail.com>
2021-10-22 14:50:48 -0600

    Added the titles for the readme file. Starting the prompt function

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 // TODO: Include packages needed for this application
 fs = require("fs");
 inquirer = require("inquirer");
+import generateMarkdown from "./utils/generateMarkdown";
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["What is the title of your project?", "What license would you like to use?", "Enter a description for the application.", "How is the application installed?", "How is the application used?", "How should someone contribute to the application?", "How should someone test the application?"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    .prompt{
+        
+    }
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,6 +13,21 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+## Description
+
+## Table of Contents
+
+## Installation
+
+## Usage
+
+## License
+
+## Contributing
+
+## Tests
+
+## Questions
 
 `;
 }

31dee068afd879b9788ae36d81cbb03838e0a8cc
zachary <zacharyberdell@gmail.com>
2021-10-22 16:27:37 -0600

    Questions asked in the prompt.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 fs = require("fs");
 inquirer = require("inquirer");
-import generateMarkdown from "./utils/generateMarkdown";
+//import generateMarkdown from "./utils/generateMarkdown";
 
 // TODO: Create an array of questions for user input
 const questions = ["What is the title of your project?", "What license would you like to use?", "Enter a description for the application.", "How is the application installed?", "How is the application used?", "How should someone contribute to the application?", "How should someone test the application?"];
@@ -11,9 +11,28 @@ function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    .prompt{
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: questions[0],
+            name: 'title'
+        },
+        {
+            type: 'list',
+            message: questions[1],
+            name: 'license',
+            choices: ['GNU', 'MIT', 'Apache', 'BSD']
+        },
+        {
+            type: 'input',
+            message: questions[2],
+            name: 'description'
+        },
+        {
+
+        }
+    ]);
         
-    }
 }
 
 // Function call to initialize app

3f4f744da6dd630a4318fa6712477405b283de3a
zachary <zacharyberdell@gmail.com>
2021-10-24 11:01:55 -0600

    Working on the logic for rendering the license section.

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,31 +10,61 @@ function renderLicenseLink(license) {}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  licenseSection = `${data.license}
+
+  Copyright ${data.githubUser}
+  
+  `
+
+  if(data.license === "GNU"){
+
+  }
+  else if(data.license === "MIT") {
+
+  }
+  else if(data.license === "Apache"){
+
+  }
+  else if(data.license === "BSD"){
+
+  }
+  else{
+
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-## Description
+  return `# ${data.title} ${renderLicenseBadge}
+
+  ## Description
 ${data.description}
 
 ## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
 
-## Installation
+## Installation #installation
 ${data.installation}
 
-## Usage
+## Usage #usage
 ${data.use}
 
-## License
+## License #license
+${renderLicenseSection}
 
-## Contributing
+## Contributing #contributing
 ${data.contribution}
 
-## Tests
+## Tests #tests
 ${data.tests}
 
-## Questions
+## Questions #questions
 
 `;
 }

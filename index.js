9b0b484e6bb5c2039995d072fe784b3178440d93
zachary <zacharyberdell@gmail.com>
2021-10-25 10:07:47 -0600

    Made code more readable. Testing how passing the data from the prompt to other functions would work.

--- index.js
+++ index.js
@@ -4,51 +4,53 @@ inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = ["What is the title of your project?", "What license would you like to use?", "Enter a description for the application.", "How is the application installed?", "How is the application used?", "How should someone contribute to the application?", "How should someone test the application?"];
+const questions = [
+    {
+        type: 'input',
+        message: "What is the title of your project?",
+        name: 'title'
+    },
+    {
+        type: 'list',
+        message: "What license would you like to use?",
+        name: 'license',
+        choices: ['GNU', 'MIT', 'Apache', 'BSD']
+    },
+    {
+        type: 'input',
+        message: "Enter a description for the application.",
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: "How is the application installed?",
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: "How is the application used?",
+        name: 'use'
+    },
+    {
+        type: 'input',
+        message: "How should someone contribute to the application?",
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: "How should someone test the application?",
+        name: 'testing'
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: questions[0],
-            name: 'title'
-        },
-        {
-            type: 'list',
-            message: questions[1],
-            name: 'license',
-            choices: ['GNU', 'MIT', 'Apache', 'BSD']
-        },
-        {
-            type: 'input',
-            message: questions[2],
-            name: 'description'
-        },
-        {
-            type: 'input',
-            message: questions[3],
-            name: 'installation'
-        },
-        {
-            type: 'input',
-            message: questions[4],
-            name: 'use'
-        },
-        {
-            type: 'input',
-            message: questions[5],
-            name: 'contribution'
-        },
-        {
-            type: 'input',
-            message: questions[6],
-            name: 'testing'
-        }
-    ]);
+    inquirer.prompt(questions).then((data) => {
+        console.log(data);
+    });
         
 }
 
